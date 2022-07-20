b5061e1f90083c342fbcd0abb9aedd67d9fd695e
FerFlores27 <ferflores27@comunidad.unam.mx>
2022-07-20 12:56:26 -0500

    clase 2 pseudocodigo

--- solutions/class1/playlist.rb
+++ solutions/class1/playlist.rb
@@ -1,5 +1,14 @@
-song = Struct.new(:name, :artist)
+Song = Struct.new(:name, :artist)
+playlist = [Song.new("as it was","harry styles"),Song.new("wait a minute","Willow")]
+
+def mostrarcanciones(p) 
+	p.each_with_index do |song,idx|
+		puts "#{idx} - #{song.name} by #{song.artist}"
+	end
+end
+
 puts "Hello! What is your name?"
 #puts escribe en la pantalla
 username = gets.chomp.to_s
 puts "Welcome #{username}"
+mostrarcanciones(playlist)
